11fc07b6066cf51934517ca1da80580552a40c37
davidjimenez99 <djresendiz1999@gmail.com>
2019-11-20 15:31:49 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#host = '172.17.64.148'
-host = '172.32.152.0'
+host = '172.32.152.32'
 port = 4196
 try:
     client.connect((host, port))
 except socket.error as exc:
     print("En este momento no se puede conectar al juego")
     exit()
+
+data = client.recv(1024)
+print(data.decode())
+
 data = client.recv(1024)
 print(data.decode())
 
@@ -19,48 +22,44 @@ for x in range(2):
     cards.append(data.decode())
     print(data.decode())
 
-#print("Tus cartas son: ", cards)
-
 while True:
+
     puntos=0
 
     if 'A' in cards:
         cards.append(cards.pop(cards.index('A')))
 
     for card in cards:
-        if card=='J' or card=='Q' or card=='K':
-            puntos+=10
-        elif card=='A':
-            if puntos<=10:
-                puntos+=11
+        if card == 'J' or card == 'Q' or card == 'K':
+            puntos += 10
+        elif card == 'A':
+            if puntos <= 10:
+                puntos += 11
             else:
-                puntos+=1
+                puntos += 1
         else:
-            puntos+=int(card)
+            puntos += int(card)
 
     print("Tus cartas son: ", cards, " = ", puntos)
 
-    st=input("hit\t\t|\t\tstay\n")
-    client.send(st.encode())
+    if puntos < 17:
+        client.send("hit".encode())
+    else:
+        client.send("stay".encode())
 
     data=client.recv(1024)
     data=data.decode()
 
-    if data=="Acción incorrecta":
-        print("Ingresa comando válido")
-        #continue
-    elif data=="stay":
-        print("Tus cartas son: ", cards, "= ", puntos)
+    if data=="stay":
+        #print("Tus cartas son: ", cards, "= ", puntos)                      #QUITAR
         #exit()                                                              #Recibir info si ganó o perdió
         break
     else:
         cards.append(data)
         print(data)
-        #print("Tus cartas son: ", cards)
 
 data=client.recv(1024)
 print(data.decode())
     
     
-
 client.close()
--- server.py
+++ server.py
@@ -7,8 +7,8 @@ print(hostName, hostIp)
 server.bind((hostName, 4196))
 server.listen(3)
 
-deck=['A','2','3','4','5','6','7','8','9','10','J','Q','K']*16                 # *4 un deck        *16 cuatro decks
-clients=[]
+deck = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']*16                 # *4 un deck        *16 cuatro decks
+clients = []
 
 game=True
 
@@ -18,133 +18,110 @@ while game:
     clients.append(connection)
     connection, address = server.accept()
     clients.append(connection)
-
-    server.settimeout(1)                                                    #CAMBIAR A 30
-    connection=None
+        
+    server.settimeout(5)                                                    #CAMBIAR A 30
+    connection = None
     try:
         connection, address = server.accept()
     except socket.error as exc:
         pass
     server.settimeout(None)
-
     if connection is not None:
         clients.append(connection)
 
-    st='Welcome to Blackjack!'
-
-    for c in clients:
-        c.send(st.encode())
-
+    for i, c in enumerate(clients):
+        c.send('Welcome to Blackjack!'.encode())
+        time.sleep(1)
+        msg = 'Se te ha asignado el turno '+str(i+1)
+        c.send(msg.encode())
     
- 
-
-
-    cards=[[]for y in clients]
+    cards = [[]for y in clients]
     #print(cards)
 
-
-    contCl=1
+    contCl = 1
     for c in clients:
 
         for x in range(2):
-            rand=int(random.randint(0,(len(deck)-1)))
-            card=deck[rand]
+            rand = int(random.randint(0,(len(deck)-1)))
+            card = deck[rand]
             print("Jugador",contCl,": ",card)
             deck.remove(card)
-
             cards[contCl-1].append(card)
-
             c.send(card.encode())
-            time.sleep(1)
-        contCl+=1
+            time.sleep(2)
+        contCl += 1
+        time.sleep(2)
 
-        time.sleep(1)
+    contCl = 1
 
-
-
-    #print(cards)
-
-        #data=connection.recv(1024)
-        #while data is not None:
-    contCl=1
-    for c in clients:
+    for i, c in enumerate(clients):
 
         while True:
             try:
-                data=c.recv(1024)
+                data = c.recv(1024)
             except socket.error as exc:
-                print("Jugador se ha desconectado")
+                print('Jugador se ha desconectado')
                 break
             #print("ENTRO A WHILE DATA!=NONE")
 
-            inp=data.decode()
-            if inp=="hit":
-                rand=int(random.randint(0,(len(deck)-1)))
-                card=deck[rand]
-                print("Jugador",contCl,": ",card)
+            inp = data.decode()
+            if inp == 'hit':
+                rand = int(random.randint(0,(len(deck)-1)))
+                card = deck[rand]
+                print('Jugador',contCl,': ',card)
                 deck.remove(card)
                 cards[contCl-1].append(card)
                 c.send(card.encode())
-                time.sleep(1)
+                time.sleep(2)
 
-            elif inp=="stay":
-                st="stay"
+            else:
+                st = 'stay'
+                print('El jugador', (i+1), 'se planta.')
                 c.send(st.encode())
                 #c.close()
+                time.sleep(2)
                 break
-                #time.sleep(1)
 
-            else:
-                st="Acción incorrecta"
-                c.send(st.encode())
-                time.sleep(1)
+            data = None
 
-            data=None
-        contCl+=1
+        contCl += 1
 
+    puntos = [0 for x in clients]
+    for i, c in enumerate(clients):
+        if 'A' in cards[i]:
+            cards[i].append(cards[i].pop(cards[i].index('A')))
 
-    puntos=[0 for x in clients]
-    contCl=1
-    for c in clients:
-        if 'A' in cards[contCl-1]:
-            cards[contCl-1].append(cards[contCl-1].pop(cards[contCl-1].index('A')))
-
-        for card in cards[contCl-1]:
-            if card=='J' or card=='Q' or card=='K':
-                puntos[contCl-1]+=10
-            elif card=='A':
-                if puntos[contCl-1]<=10:
-                    puntos[contCl-1]+=11
+        for card in cards[i]:
+            if card == 'J' or card == 'Q' or card == 'K':
+                puntos[i] += 10
+            elif card == 'A':
+                if puntos[i] <= 10:
+                    puntos[i] += 11
                 else:
-                    puntos[contCl-1]+=1
+                    puntos[i] += 1
             else:
-                puntos[contCl-1]+=int(card)
+                puntos[i] += int(card)
 
-
-    #print(puntos)
-    perd=[]
-    gan=[]
+    print('puntos:',puntos)           ############################
+    perd = []
+    gan = []
     for x, punt in enumerate(puntos):
         if punt > 21:
             perd.append(x)
         elif punt <= 21:
             gan.append(x)               #ARREGLAR GANADOR
 
-    perdedor="PERDISTE"
-    ganador="GANADOR"
+    print('perdedor:',perd)           ############################
+    print('ganador:',gan)             ############################
+
     for x, c in enumerate(clients):
         if x in perd:
             #print(x,c,perdedor)
-            c.send(perdedor.encode())
+            c.send('PERDISTE'.encode())
         else:
-            c.send(ganador.encode())
-
-
-    print("SALIO DEL WHILE")                                            #COMPARAR PUNTOS DE JUGADORES Y DECIR GANADOR
-
-    #print(cards)
+            c.send('GANADOR'.encode())
+            #print('Jugador', (i+1), 'ha ganado!')
 
     game=False
 
-
-print("ACABÓ")
+print("EL JUEGO TERMINÓ")
